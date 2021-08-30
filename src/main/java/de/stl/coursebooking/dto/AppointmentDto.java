8155c5a28c9e0dea45bc9761f74320f03f8f29b5
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-08-30 19:50:05 +0200

    compose appointment

--- src/main/java/de/stl/coursebooking/dto/AppointmentDto.java
+++ src/main/java/de/stl/coursebooking/dto/AppointmentDto.java
@@ -1,4 +1,84 @@
 package de.stl.coursebooking.dto;
 
+import java.util.Date;
+import java.util.List;
+
 public class AppointmentDto {
+    private String student;
+    private String lecturer;
+    private List<String> participants;
+    private String description;
+    private Date startsAt;
+    private Date endsAt;
+
+    public AppointmentDto() {}
+
+    public AppointmentDto(String student, String lecturer, List<String> participants, String description, Date startsAt, Date endsAt) {
+        this.student = student;
+        this.lecturer = lecturer;
+        this.participants = participants;
+        this.description = description;
+        this.startsAt = startsAt;
+        this.endsAt = endsAt;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public List<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartsAt() {
+        return startsAt;
+    }
+
+    public void setStartsAt(Date startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public Date getEndsAt() {
+        return endsAt;
+    }
+
+    public void setEndsAt(Date endsAt) {
+        this.endsAt = endsAt;
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentDto{" +
+                "student='" + student + '\'' +
+                ", lecturer='" + lecturer + '\'' +
+                ", participants=" + participants +
+                ", description='" + description + '\'' +
+                ", startsAt=" + startsAt +
+                ", endsAt=" + endsAt +
+                '}';
+    }
 }
