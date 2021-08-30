0054691e85ed5b16b68b0c22e779e4aba1a6d6dc
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-08-30 19:23:37 +0200

    wip

--- src/main/java/de/stl/coursebooking/model/Appointment.java
+++ src/main/java/de/stl/coursebooking/model/Appointment.java
@@ -37,4 +37,92 @@ public class Appointment {
         this.startsAt = startsAt;
         this.endsAt = endsAt;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public List<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public String getCancellationReason() {
+        return cancellationReason;
+    }
+
+    public void setCancellationReason(String cancellationReason) {
+        this.cancellationReason = cancellationReason;
+    }
+
+    public boolean isConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public Date getStartsAt() {
+        return startsAt;
+    }
+
+    public void setStartsAt(Date startsAt) {
+        this.startsAt = startsAt;
+    }
+
+    public Date getEndsAt() {
+        return endsAt;
+    }
+
+    public void setEndsAt(Date endsAt) {
+        this.endsAt = endsAt;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
